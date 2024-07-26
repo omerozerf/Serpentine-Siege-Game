90c042fd4d7f72815bfb4d33e1821baaf93ce0b9
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-07-26 18:07:12 +0300

    fix: delete logs

--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -46,14 +46,11 @@ public class SnakeController : MonoBehaviour {
     {
         transform.position += transform.forward * (_moveSpeed * Time.deltaTime);
 
-        // Steer
-        float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
+        float steerDirection = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * (steerDirection * _steerSpeed * Time.deltaTime));
 
-        // Store position history
         m_PositionsHistory.Insert(0, transform.position);
 
-        // Move body parts
         int index = 0;
         foreach (GameObject body in m_BodyParts)
         {
@@ -61,11 +58,9 @@ public class SnakeController : MonoBehaviour {
             
             Vector3 point = m_PositionsHistory[Mathf.Clamp(index * _gap, 0, m_PositionsHistory.Count - 1)];
 
-            // Move body towards the point along the snakes path
             Vector3 moveDirection = point - body.transform.position;
             body.transform.position += moveDirection * (_bodySpeed * Time.deltaTime);
 
-            // Rotate body towards the point along the snakes path
             body.transform.LookAt(point);
 
             index++;
@@ -74,8 +69,6 @@ public class SnakeController : MonoBehaviour {
 
     private void GrowSnake() 
     {
-        // Instantiate body instance and
-        // add it to the list
         GameObject body = Instantiate(_bodyPrefab);
         m_BodyParts.Add(body);
     }
