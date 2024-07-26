e3c394878a980aa76ef04ec80f4ab7f584b905f3
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-07-26 16:05:36 +0300

    fix: snake move

--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -1,29 +1,63 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class SnakeController : MonoBehaviour
-{
-    [SerializeField] private float _speed;
-    [SerializeField] private int _gap;
-    [SerializeField] private List<Transform> _bodyPartTransformList;
+public class SnakeController : MonoBehaviour {
+
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _steerSpeed = 180;
+    [SerializeField] private float _bodySpeed = 5;
+    [SerializeField] private int _gap = 10;
+    [SerializeField] private GameObject _bodyPrefab;
     
-    private readonly List<Vector3> m_PositionHistoryList = new List<Vector3>();
+    private readonly List<GameObject> m_BodyParts = new List<GameObject>();
+    private readonly List<Vector3> m_PositionsHistory = new List<Vector3>();
 
-
-    private void Update()
+    private void Start() 
     {
-        transform.Translate(transform.forward * (_speed * Time.deltaTime));
-        
-        m_PositionHistoryList.Insert(0, transform.position);
-        
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+    }
+
+    private void Update() 
+    {
+        transform.position += transform.forward * (_moveSpeed * Time.deltaTime);
+
+        // Steer
+        float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
+        transform.Rotate(Vector3.up * (steerDirection * _steerSpeed * Time.deltaTime));
+
+        // Store position history
+        m_PositionsHistory.Insert(0, transform.position);
+
+        // Move body parts
         int index = 0;
-        foreach (Transform bodyPartTransform in _bodyPartTransformList)
+        foreach (GameObject body in m_BodyParts)
         {
-            Vector3 point = m_PositionHistoryList[Math.Min(index * _gap, m_PositionHistoryList.Count - 1)];
-            bodyPartTransform.position = point;
+            if (!body) continue;
+            
+            Vector3 point = m_PositionsHistory[Mathf.Clamp(index * _gap, 0, m_PositionsHistory.Count - 1)];
+
+            // Move body towards the point along the snakes path
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * (_bodySpeed * Time.deltaTime);
+
+            // Rotate body towards the point along the snakes path
+            body.transform.LookAt(point);
+
             index++;
         }
+    }
+
+    private void GrowSnake() 
+    {
+        // Instantiate body instance and
+        // add it to the list
+        GameObject body = Instantiate(_bodyPrefab);
+        m_BodyParts.Add(body);
     }
 }
