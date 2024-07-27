9eacad811336ee4e964533601175465d22183acc
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-07-27 16:32:59 +0300

    fix: snake movement

--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -5,7 +5,6 @@ using UnityEngine.Serialization;
 
 public class SnakeController : MonoBehaviour {
 
-    [SerializeField] private float _moveSpeed = 5;
     [SerializeField] private float _steerSpeed = 180;
     [SerializeField] private float _bodySpeed = 5;
     [SerializeField] private int _gap = 10;
@@ -38,14 +37,57 @@ public class SnakeController : MonoBehaviour {
         GrowSnake();
         GrowSnake();
         GrowSnake();
+        GrowSnake();GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
         GrowSnake();
 
     }
 
     private void Update() 
     {
-        transform.position += transform.forward * (_moveSpeed * Time.deltaTime);
-
         float steerDirection = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * (steerDirection * _steerSpeed * Time.deltaTime));
 
