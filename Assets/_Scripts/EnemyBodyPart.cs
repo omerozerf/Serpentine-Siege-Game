74d0c89acc66bf449f5e743076943cdbb25514cd
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-07-27 20:27:13 +0300

    refactor: snake movement

---
+++ Assets/_Scripts/EnemyBodyPart.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class EnemyBodyPart : MonoBehaviour
+{
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+}
---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _turnSpeed = 100;
+    [SerializeField] private float _turnInterval = 1;
+
+    private float m_NextTurnTime;   
+    private Quaternion m_TargetRotation;
+    private int m_Flag;
+
+    private void Start()
+    {
+        SetRandomRotation();           
+    }
+
+    void Update()
+    {
+        MoveForward();                 
+        HandleTurning();               
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * (_moveSpeed * Time.deltaTime));
+    }
+
+    void HandleTurning()
+    {
+        if (Time.time >= m_NextTurnTime)
+        {
+            SetRandomRotation();
+            m_NextTurnTime = Time.time + _turnInterval;
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, m_TargetRotation, _turnSpeed * Time.deltaTime);
+    }
+
+    void SetRandomRotation()
+    {
+        // Y ekseni etrafında -90 ile 90 derece arasında rastgele bir açı belirle
+        float randomYRotation;
+        if (m_Flag % 2 == 0)
+        { 
+            randomYRotation = 270;
+        }
+        else
+        {
+            randomYRotation = 90;
+        }
+        m_Flag++;
+        
+        m_TargetRotation = Quaternion.Euler(0, randomYRotation, 0);
+    }
+}
