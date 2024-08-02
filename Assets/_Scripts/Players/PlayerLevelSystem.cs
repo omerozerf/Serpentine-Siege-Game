9c8b5080d032b10b44b63e12b02028d5ba12aeda
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-08-02 17:34:28 +0300

    feat: level system

--- Assets/_Scripts/Players/PlayerLevelSystem.cs
+++ Assets/_Scripts/Players/PlayerLevelSystem.cs
@@ -9,14 +9,51 @@ namespace Players
         
         public static event Action<int> OnLevelUp; 
         
-        private int m_Level = 1;
+        [SerializeField] private int m_Level = 1;
+        [SerializeField] private int m_Experience = 0;
+        [SerializeField] private int m_ExperienceToNextLevel = 100; // İlk level için gerekli deneyim puanı
+        [SerializeField] private float experienceMultiplier = 1.5f; // Her levelde gerekli deneyim artış katsayısı
         
-        
-        public void LevelUp()
+        public void GainExperience(int amount)
+        {
+            m_Experience += amount;
+            CheckLevelUp();
+        }
+
+        private void CheckLevelUp()
+        {
+            while (m_Experience >= m_ExperienceToNextLevel)
+            {
+                m_Experience -= m_ExperienceToNextLevel;
+                LevelUp();
+            }
+        }
+
+        private void LevelUp()
         {
             m_Level++;
-            
+            m_ExperienceToNextLevel = Mathf.RoundToInt(m_ExperienceToNextLevel * experienceMultiplier); // Her levelde gerekli deneyimi arttır
             OnLevelUp?.Invoke(m_Level);
+        }
+
+        public int GetLevel()
+        {
+            return m_Level;
+        }
+
+        public int GetExperience()
+        {
+            return m_Experience;
+        }
+
+        public int GetExperienceToNextLevel()
+        {
+            return m_ExperienceToNextLevel;
+        }
+
+        public float GetExperienceMultiplier()
+        {
+            return experienceMultiplier;
         }
     }
 }
