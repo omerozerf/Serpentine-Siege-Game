6560f924b26314101f236d23eb24f9e072e360e5
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-08-06 12:41:48 +0300

    fix: level up manager

--- Assets/_Scripts/_Managers/LevelUpManager.cs
+++ Assets/_Scripts/_Managers/LevelUpManager.cs
@@ -1,6 +1,7 @@
 ﻿using Players;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 namespace _Managers
 {
@@ -13,7 +14,6 @@ namespace _Managers
         
         private static LevelUpManager ms_Instance;
 
-
         private void Awake()
         {
             ms_Instance = this;
@@ -23,7 +23,6 @@ namespace _Managers
             {
                 _levelUpParentTransform.gameObject.SetActive(false);
                 GameManager.SetIsPaused(false);
-
             });
             
             _levelUpRightButton.GetButton().onClick.AddListener(() =>
@@ -41,13 +40,39 @@ namespace _Managers
             _levelUpRightButton.GetButton().onClick.RemoveAllListeners();
         }
 
-
         private void OnLevelUp(int level)
         {
             Debug.Log("Level Up! New Level: " + level);
-         
             GameManager.SetIsPaused(true);
+            
+            ChangePowerUp();
+            
             _levelUpParentTransform.gameObject.SetActive(true);
+        }
+
+        public static void ChangePowerUp()
+        {
+            LevelUpPowerUpSO[] powerUps = ms_Instance._levelUpPowerUpSoArray;
+
+            // Ensure we have at least two different types
+            if (powerUps.Length < 2)
+            {
+                Debug.LogError("Not enough power-ups to select two different types.");
+                return;
+            }
+
+            LevelUpPowerUpSO leftPowerUp = null;
+            LevelUpPowerUpSO rightPowerUp = null;
+
+            while (leftPowerUp == null || rightPowerUp == null ||
+                   leftPowerUp.GetPowerUpType() == rightPowerUp.GetPowerUpType())
+            {
+                leftPowerUp = powerUps[Random.Range(0, powerUps.Length)];
+                rightPowerUp = powerUps[Random.Range(0, powerUps.Length)];
+            }
+
+            ms_Instance._levelUpLeftButton.SetLevelUpPowerUpSO(leftPowerUp);
+            ms_Instance._levelUpRightButton.SetLevelUpPowerUpSO(rightPowerUp);
         }
     }
 }
